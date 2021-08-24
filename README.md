84477378096ffbaa9439b4c832432807ed0b3dec
divluffy <div.jo2022@gmail.com>
2021-08-24 15:04:22 +0300

    files strcture

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # team-1
+### let start
---
+++ server/index.js
@@ -0,0 +1,5 @@
+const app = require('./app.js')
+
+app.listen(app.get('port'), () => {
+    console.log(`App running http://localhost:${app.get('port')}`);
+});
