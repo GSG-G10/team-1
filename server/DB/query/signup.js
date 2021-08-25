72a0c357a5a21a3d0b20b974d48c44402f0a471d
EmanAmassi <eman.amassi98@gmail.com>
2021-08-26 00:55:10 +0300

    add sign up work

--- server/DB/query/signup.js
+++ server/DB/query/signup.js
@@ -1,9 +1,10 @@
 const connection = require('../config/connections');
 
 
-const signup = (username, password, email) => {
+const signup = (username, email, password) => {
+    console.log('bbb' + username);
     const sql = {
-        text : 'INSERT INTO users (username, password, email) RETURNING *',
+        text : 'INSERT INTO users (username, password, email) VALUES($1,$2,$3) RETURNING *',
         values: [username, password, email],
     };
 
