9320a50e99bdd481e008d9006d04b2ad990f949a
divluffy <div.jo2022@gmail.com>
2021-08-26 02:58:22 +0300

    code edit watch

--- server/router/login/login.js
+++ server/router/login/login.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const routerLogin = express.Router()
 
-routerLogin.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname,'../../public/login/index.html'))
-})
 
+routerLogin.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'../../../public/login/index.html'))
+})
 
 routerLogin.post('/', (req, res)=>{
     console.log(req.body);
