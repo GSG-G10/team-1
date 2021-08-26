f8bca28b8e27818affca14541fc118126d0a86a0
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-08-26 08:45:33 +0300

    solve an error and use eslint to format the code #25

--- server/DB/query/add-video-query.js
+++ server/DB/query/add-video-query.js
@@ -1,11 +1,11 @@
 const connection = require('../config/connections');
 
 const addVideoQuery = (data) => {
-    const dataArray = [data.username, data.urlVideo, data.discriptionVideo, data.title, data.urlImg, data.category, data.At];
-    return connection.query(
-        'INSERT INTO customers (created_by, url, description, title, img, tag, created_at) VALUES ($1 ,$2 ,$3 ,$4 ,$5 ,$6, $7);',
-        dataArray,
-    );
+  const dataArray = [1, data.urlVideo, data.descriptionVideo, data.title, data.urlImage, data.category];
+  return connection.query(
+    'INSERT INTO video (created_by, url, description, title, img, tag) VALUES ($1 ,$2 ,$3 ,$4 ,$5 ,$6);',
+    dataArray,
+  );
 };
 
 module.exports = addVideoQuery;
