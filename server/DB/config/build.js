7a1d7a31c48b06571450d6b482f1ebb72079e5d0
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-08-26 14:33:48 +0300

    edit connection and packege json for heroku #10

--- server/DB/config/build.js
+++ server/DB/config/build.js
@@ -4,7 +4,7 @@ const connection = require("./connections");
 let runBuild = () => {
   let sql = readFileSync(join(__dirname, "build.sql")).toString();
   //if we have multiple environments we will add it just in test env case!
-  sql += readFileSync(join(__dirname, "build.sql")).toString();
+  sql += readFileSync(join(__dirname, "insert.sql")).toString();
   connection
     .query(sql)
     .then(() => console.log("build created successfully!"))
